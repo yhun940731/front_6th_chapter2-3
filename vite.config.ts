f0363c0e9306b26b445a825b541c81202c41b9aa
최용훈 <yonghun@douzone.com>
2025-08-14 15:12:46 +0900

    Deploy: deploy test.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        // target: 'https://jsonplaceholder.typicode.com',
-        target: "https://dummyjson.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
       },
     },
   },
-})
+});
