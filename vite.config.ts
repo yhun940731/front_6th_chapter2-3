8c49a6a10971c60ea55c55eef3373b8271d5d8d4
최용훈 <yonghun@douzone.com>
2025-08-15 14:19:25 +0900

    Fix: Server proxy 설정.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        // target: 'https://jsonplaceholder.typicode.com',
+        target: 'https://dummyjson.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   base: './',
   build: {
     rollupOptions: {
